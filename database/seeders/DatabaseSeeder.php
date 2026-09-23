4bcb1ac12c9d623e6e2330b75c92b525912d27f8
Franck56795 <foalengfranck6@gmail.com>
2026-09-23 03:01:53 -0700

    Mise à jour du DatabaseSeeder pour l'admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,13 +17,23 @@ class DatabaseSeeder extends Seeder
             
 
         // Création de votre compte Super Admin (déjà vérifié)
-        User::create([
+        /*User::updateOrCreate([
             'name' => 'Foaleng Neumann',
             'email' => 'foalengfranck6@gmail.com',
-            'password' => Hash::make('pogba@21'),
+            'password' => Hash::make('MaitrePlombier2026@'),
             'is_admin' => true,
             'email_verified_at' => now(), // Empêche le blocage 403
-        ]);
+        ]);*/
+
+        User::updateOrCreate(
+                ['email' => 'foalengfranck6@gmail.com'], // Condition pour chercher si l'utilisateur existe
+                [
+                    'name' => 'Foaleng Neumann',
+                    'password' => Hash::make('MaitrePlombier2026@'),
+                    'is_admin' => true,
+                    'email_verified_at' => now(),
+                ]
+  );
     }
 
 
